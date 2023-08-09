bf73c478c8141be8e0f518934525c2e76ab6e131
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2023-08-09 18:15:30 +0530

    created cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,11 +15,24 @@ const getTrendingAnime = async () => {
     return [];
   }
 };
+const getRecentAnime = async () => {
+  try {
+    const response = await axios.get("https://animetrix-api.vercel.app/meta/anilist/recent-episodes", {
+      responseType: 'json'
+    });
+
+    return response.data.results; 
+  } catch (error) {
+    console.error("Error fetching recent anime:", error);
+    return [];
+  }
+};
 export default async function page () {
-  const posts = await getTrendingAnime()
+  const Trending = await getTrendingAnime()
+  const Recent = await getRecentAnime()
   return (
     <div className="p-4 pb-40 md:pb-10 text-xl mt-24 font-semibold flex-1 h-screen">
-      <Slider posts = {posts} />
+      <Slider posts = {Trending} />
 
       <div className='flex flex-col mt-9'>
         <div className="flex justify-between items-center">
@@ -27,37 +40,10 @@ export default async function page () {
           <Link href={"/recent-anime"}>Load more</Link>
         </div>
         <div className='flex gap-2'>
-          <Cards/>
+          <Cards props = {Recent}/>
         </div>
       </div>
 
-
-      <div className='flex flex-col mt-9 md:pb-10'>
-        <div className="flex justify-between items-center">
-          <h1 className='text-3xl lg:text-5xl font-bold'>Popular</h1>
-          <Link href={"/popular-anime"}>Load more</Link>
-        </div>
-        <div className='flex mt-9 gap-7 overflow-x-scroll'>
-          {
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => (
-             <Cards/>
-            ))
-          }
-        </div>
-      </div>
-
-      <div className='flex flex-col mt-9  md:pb-10'>
-        <div className="flex justify-between items-center">
-          <h1 className='text-3xl lg:text-5xl font-bold'>Recommended</h1>
-        </div>
-        <div className='flex mt-9 gap-7 overflow-x-scroll'>
-          {
-            [1].map(() => (
-             <Cards/>
-            ))
-          }
-        </div>
-      </div>
 
     </div>
   )
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
 
-const Cards = () => {
-  return (
-    <div>Cards</div>
-  )
+interface Anime {
+  image: string;
+  title: {
+    userPreferred?: string;
+    english?: string;
+    romaji?: string;
+    native?: string;
+  };
 }
 
-export default Cards
+interface CardsProps {
+  props: Anime[];
+}
+
+const Cards: React.FC<CardsProps> = ({ props }) => {
+  return (
+    <div className='flex gap-4 overflow-x-auto mt-9 lg:grid lg:grid-cols-7 lg:overflow-x-hidden'>
+      {props.map((anime) => {
+        return (
+          <div className='flex flex-col gap-2' key={anime.title.userPreferred || anime.title.english || anime.title.romaji || anime.title.native}>
+            <img
+              src={anime.image}
+              className='rounded-lg aspect-auto w-[400px] bg-cover'
+              alt="this is an image"
+            />
+            <span className='truncate w-32 text-lg text-center m-auto flex gap-0 items-center'>
+              {anime.title.userPreferred || anime.title.english || anime.title.romaji || anime.title.native}
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Cards;
