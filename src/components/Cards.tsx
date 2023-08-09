1f24ecb341798f8c6cb71043ada4678558708f26
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2023-08-09 22:37:17 +0530

    Made card client component and added mouse movement functions

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+"use client"
+import React, { useRef, useState } from 'react';
 import Image from 'next/image';
+
 interface Anime {
   image: string;
-  id : number;
+  id: number;
   title: {
     userPreferred?: string;
     english?: string;
@@ -16,21 +18,66 @@ interface CardsProps {
 }
 
 const Cards: React.FC<CardsProps> = ({ props }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [dragStartX, setDragStartX] = useState(0);
+  const [scrollStartX, setScrollStartX] = useState(0);
+
+  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
+    setIsDragging(true);
+    setDragStartX(event.clientX);
+    setScrollStartX(containerRef.current!.scrollLeft);
+  };
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (!isDragging) return;
+
+    const dx = event.clientX - dragStartX;
+    containerRef.current!.scrollLeft = scrollStartX - dx;
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  const handleMouseLeave = () => {
+    if (isDragging) {
+      setIsDragging(false);
+    }
+  };
+
   return (
-    <div className='flex flex-1 gap-3 overflow-x-auto mt-9 lg:grid
-     lg:grid-cols-8 lg:overflow-x-hidden'>
+    <div
+      className='flex gap-3 overflow-x-auto mt-9 lg:grid lg:grid-flow-col-dense'
+      ref={containerRef}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseLeave}
+    >
       {props?.map((anime) => {
         return (
-          <div className='flex flex-col gap-4  rounded-lg ' key={`${anime.id + 1}`}>
+          <div
+            key={`${anime.id + 1}`}
+            className='flex flex-col gap-4 rounded-lg cursor-grab'
+            onMouseDown={handleMouseDown}
+          >
             <Image
               src={anime?.image}
-              className='rounded-lg h-full duration-200 hover:scale-105 '
-              alt={`an image of ${anime?.title?.userPreferred || anime?.title?.english || anime?.title?.romaji || anime.title?.native}`}
+              className='rounded-lg h-full duration-200 hover:scale-105'
+              alt={`an image of ${
+                anime?.title?.userPreferred ||
+                anime?.title?.english ||
+                anime?.title?.romaji ||
+                anime.title?.native
+              }`}
               height={200}
               width={600}
             />
-            <span className='truncate w-32 lg:w-44 p-2 text-sm md:text-xl pb-5 capitalize'>
-              {anime?.title?.userPreferred || anime?.title?.english || anime?.title?.romaji || anime?.title?.native}
+            <span className='truncate w-32 lg:w-44 p-2 text-sm md:text-xl lg:text-lg pb-5 capitalize'>
+              {anime?.title?.userPreferred ||
+                anime?.title?.english ||
+                anime?.title?.romaji ||
+                anime?.title?.native}
             </span>
           </div>
         );
