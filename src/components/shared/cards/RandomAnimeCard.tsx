71ea5ec508f70cb88b5ecfbb3705d9e63fa71dd5
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2023-08-28 01:27:59 +0530

    fixed random anime card

--- src/components/shared/cards/RandomAnimeCard.tsx
+++ src/components/shared/cards/RandomAnimeCard.tsx
@@ -12,14 +12,14 @@ export async function RandomAnimeCard() {
     }
 
     return (
-        <div className="flex justify-center items-center flex-col mt-5">
+        <div className="flex flex-col mt-5 justify-start">
             <Link href={`/details/${randomAnime.id}`} className="content-normal w-full h-full">
                 <div className="md:w-48 md:h-64 h-56 w-40 relative">
                     <img src={randomAnime?.image} alt={`an image of ${randomAnime?.title?.userPreferred || randomAnime?.title?.english || randomAnime?.title?.romaji || randomAnime.title?.native}`} className="rounded-lg hover:scale-105 duration-200 md:w-48 md:h-64 h-56 w-40" draggable={false} />
                 </div>
             </Link>
-            <h1 className=" truncate w-[70%] m-auto text-center mb-2">{randomAnime?.title?.userPreferred || randomAnime?.title?.english || randomAnime?.title?.romaji || randomAnime?.title?.native?.toLocaleLowerCase()}</h1>
-            <div className="flex gap-3 text-sm w-[70%] justify-center items-center truncate m-auto">
+            <h1 className=" truncate w-40 mb-2 justify-start">{randomAnime?.title?.userPreferred || randomAnime?.title?.english || randomAnime?.title?.romaji || randomAnime?.title?.native?.toLocaleLowerCase()}</h1>
+            <div className="flex gap-3 text-sm truncate">
                 <span>{randomAnime?.type}</span>
                 <span>{randomAnime?.subOrDub?.toLocaleUpperCase()}</span>
             </div>
