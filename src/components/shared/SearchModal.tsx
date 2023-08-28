4aae3886f5cdcf8b2607a54102fac0dcc7480ae0
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2023-08-29 00:40:34 +0530

    changed design of search modal

--- src/components/shared/SearchModal.tsx
+++ src/components/shared/SearchModal.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useRef, useCallback, useState, Suspense } from "react
 import { Search } from "lucide-react";
 import Anime from "@/types/animetypes";
 import SearchCards from "./cards/SearchCards";
+import SpinLoading from "../loading/SpinLoading";
 const SearchModal = ({ trending }: { trending: Anime[] }) => {
     const modalRef = useRef<HTMLDivElement>(null);
     const [openSearch, setOpenSearch] = useState<boolean>(false);
@@ -53,15 +54,15 @@ const SearchModal = ({ trending }: { trending: Anime[] }) => {
     return (
         <>
             <Search className="cursor-pointer" onClick={() => setOpenSearch(!openSearch)} />
-            <div className={`bg-black/25 overscroll-none w-screen backdrop-blur h-screen fixed p-4 left-0 top-0 bottom-0 duration-200 right-0 ${openSearch ? "scale-100" : "scale-0"}`}>
+            <div className={`bg-black/80 overscroll-none w-screen backdrop-blur-xl h-screen fixed p-4 left-0 top-0 bottom-0 duration-200 right-0 ${openSearch ? "scale-100" : "scale-0"}`}>
                 <div className="flex h-screen items-center right-5 top-5">
-                    <div className="bg-white/75 h-96 overflow-y-scroll  gap-4 flex-col rounded-lg p-4 max-w-3xl m-auto w-screen text-black flex" ref={modalRef}>
-                        <h1 className="font-bold text-lg flex gap-3 items-center">
-                            Open/Close : <span className=" bg-black text-white p-2 rounded-lg text-sm">Ctrl</span>+ <span className=" bg-black text-white text-sm p-2 rounded-lg">Space</span>
+                    <div className="bg-black border-2 border-white/40 h-96 overflow-y-scroll  gap-4 flex-col rounded-lg p-4 max-w-3xl m-auto w-screen text-black flex" ref={modalRef}>
+                        <h1 className="font-bold text-white text-lg flex gap-3 items-center">
+                            Open/Close : <span className=" bg-white text-black p-2 rounded-lg text-sm">Ctrl</span>+ <span className=" bg-white text-black text-sm p-2 rounded-lg">Space</span>
                         </h1>
-                        <input type="text" placeholder="I am looking for ......" className="border-2 bg-black border-none outline-none focus:outline-none p-4 w-full rounded-lg text-white" />
+                        <input type="text" placeholder="I am looking for ......" className="border-2 border-white/40 bg-transparent  outline-none focus:outline-none p-4 w-full rounded-lg text-white" />
                         <h1 className=" text-2xl font-semibold">Trending</h1>
-                        <Suspense fallback={<>Loading...</>}>
+                        <Suspense fallback={<SpinLoading />}>
                             <SearchCards results={trending} modalClose={handleModalClose} />
                         </Suspense>
                     </div>
--- src/components/shared/cards/SearchCards.tsx
+++ src/components/shared/cards/SearchCards.tsx
@@ -9,19 +9,21 @@ const SearchCards = ({ results, modalClose }: { results: Anime[]; modalClose: Mo
     return (
         <>
             {results?.length > 0 ? (
-                <div className="flex flex-col gap-6">
+                <div className="flex  text-white flex-col gap-6">
                     {results.slice(0, 10)?.map((anime, index) => (
                         <div key={index}>
                             <Link href={`/details/${anime?.id}`} className="flex overflow-hidden gap-3 items-center" onClick={modalClose}>
                                 <img src={anime.image} alt="" className=" w-28 rounded-lg" />
                                 <div className="flex flex-col gap-3 font-semibold overflow-hidden">
-                                    <h1 className=" font-semibold text-lg md:text-xl truncate w-[90%]">{anime?.title?.userPreferred || anime.title.romaji || anime.title.english || anime.title.native}</h1>
+                                    <h1 className=" font-semibold text-sm md:text-xl truncate w-[90%]">{anime?.title?.userPreferred || anime.title.romaji || anime.title.english || anime.title.native}</h1>
                                     <div className="flex flex-wrap gap-3">
                                         {anime.genres.map((genres) => (
-                                            <span key={Math.random()}>{genres}</span>
+                                            <span key={Math.random()} className="text-sm md:text-xl">
+                                                {genres}
+                                            </span>
                                         ))}
                                     </div>
-                                    <div className="flex gap-3">
+                                    <div className="flex gap-3 text-sm md:text-lg">
                                         <span>Episode : {anime.totalEpisodes || 0}</span>
                                         <span>Type : {anime.type || "Unknown"}</span>
                                     </div>
