523d8987a0d7ba995b609d5e2732e54b060faf4f
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2023-09-08 21:36:14 +0530

    passed deno

--- src/components/AniScanSearchLayout.tsx
+++ src/components/AniScanSearchLayout.tsx
@@ -13,18 +13,20 @@ interface ApiResponse {
     result: Anime[];
 }
 
-const AniScanSearchLayout: React.FC<{ searchResult: ApiResponse | null; setToggle: any }> = ({ searchResult, setToggle }) => {
+const AniScanSearchLayout: React.FC<{ searchResult: ApiResponse | null; setToggle: (value: boolean) => void }> = ({ searchResult, setToggle }) => {
     if (searchResult === null) {
         return null; // Or render an appropriate placeholder or loading indicator
     }
 
     return (
-        <div className="text-white pb-32 md:pb-0">
+        <div className="text-white grid grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 m-auto gap-5 p-4 pb-32 md:pb-0">
             {searchResult.result.map((anime, index) => (
-                <div key={index}>
+                <div key={index} className="flex flex-col rounded-lg justify-center items-center bg-white/20">
                     <img src={anime.image} alt={anime.filename} />
-                    <h1>Filename: {anime.filename}</h1>
-                    <p>Episode: {anime.episode}</p>
+                    <div className=" p-4">
+                        <h1>Filename: {anime.filename}</h1>
+                        <p>Episode: {anime.episode}</p>
+                    </div>
                     {/* Add more details if needed */}
                 </div>
             ))}
