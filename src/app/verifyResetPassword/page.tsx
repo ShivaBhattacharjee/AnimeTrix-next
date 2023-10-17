eb3579be60545bed1ec3d10329e0622c196a3393
Shiva <shivaxwarrior@gmail.com>
2023-10-17 22:25:51 +0530

    passed vercel

--- src/app/verifyResetPassword/page.tsx
+++ src/app/verifyResetPassword/page.tsx
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
-const page = () => {
+const Page = () => {
     const [token, setToken] = useState("")
     const [verified, setVerified] = useState(false)
     const [error, setError] = useState("")
@@ -36,4 +36,4 @@ const page = () => {
     )
 }
 
-export default page
+export default Page
