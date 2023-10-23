b28074ac1a1e639b570eb99ac8f13239ee07de19
Shiva <shivaxwarrior@gmail.com>
2023-10-24 00:42:10 +0530

    removed bookmark route

--- src/app/api/bookmark/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export default async function GET(request: NextRequest) {
-    const reqBody = await request.json();
-    return NextResponse.json({
-        message: "Hello from bookmark route",
-    });
-}
