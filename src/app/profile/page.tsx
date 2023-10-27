405c0c06cc528bea99af30649dcddafd482a9a3e
Shiva <itshivahere@outlook.com>
2023-10-27 19:34:46 +0530

    added history in profile page and conditonally render history and bookmark

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,101 +1,165 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { useRouter } from 'next/navigation';
-import Toast from '@/utils/toast';
-import SpinLoading from '@/components/loading/SpinLoading';
-import { getCookie } from 'cookies-next';
-import { Error } from '@/types/ErrorTypes';
-import { LogOut } from 'lucide-react';
-import Link from 'next/link';
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import Toast from "@/utils/toast";
+import SpinLoading from "@/components/loading/SpinLoading";
+import { getCookie } from "cookies-next";
+import { Error } from "@/types/ErrorTypes";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+type HistoryItem = {
+    streamId: string;
+    animeId: number;
+    image: string;
+    coverImage: string;
+    episode: string;
+    title: string;
+    _id: string;
+};
+
+type UserHistoryResponse = {
+    nextPage: boolean;
+    history: HistoryItem[];
+    userHistory: {
+        history: HistoryItem[];
+    };
+};
 const Page = () => {
-    const [userName, setUserName] = useState("")
-    const [email, setEmail] = useState("")
-    const [loading, setLoading] = useState(true)
+    const [userName, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [history, setHistory] = useState<HistoryItem[]>([]);
+    const [historyLoading, setHistoryLoading] = useState(true);
     const router = useRouter();
-    const token = getCookie("token")
+    const token = getCookie("token");
     const logout = async () => {
         try {
-            await axios.get('/api/logout')
-            Toast.SuccessshowToast("Logout Successfull")
+            await axios.get("/api/logout");
+            Toast.SuccessshowToast("Logout Successfull");
             router.refresh();
-            router.push("/")
+            router.push("/");
         } catch (error: unknown) {
-            const Error = error as Error
-            Toast.ErrorShowToast(Error?.response?.data?.error || "Something went wrong")
+            const Error = error as Error;
+            Toast.ErrorShowToast(Error?.response?.data?.error || "Something went wrong");
         }
-    }
+    };
     const getUserData = async () => {
         try {
-            const userResponse = await fetch('/api/get-users');
+            const userResponse = await fetch("/api/get-users");
             const user = await userResponse.json();
             setUserName(user?.userData?.username);
             setEmail(user?.userData?.email);
-            setLoading(false)
+            setLoading(false);
         } catch (error: unknown) {
-            const Error = error as Error
-            setLoading(false)
+            const Error = error as Error;
+            setLoading(false);
             Toast.ErrorShowToast(Error?.message || "Something went wrong");
         }
     };
+    const getUserHistory = async () => {
+        try {
+            const response = await fetch("/api/history");
+            if (token) {
+                if (!response.ok) {
+                    throw new Error("Network response error");
+                }
+            }
+            const data: UserHistoryResponse = await response.json();
+            setHistory(data?.userHistory?.history || []);
+            setHistoryLoading(false);
+            console.dir(data);
+        } catch (error: unknown) {
+            console.error(error);
+            setHistoryLoading(false);
+        }
+    };
     useEffect(() => {
-        getUserData()
-    }, [token])
+        getUserData();
+        if (token) getUserHistory();
+    }, [token]);
     return (
         <>
-            {
-                loading ? (
-                    <div className="flex justify-center items-center min-h-[100dvh]">
-                        <SpinLoading />
-                    </div>
-                ) : (
-                    <div className='min-h-[100dvh]'>
-                        <div className=' p-4 flex flex-col lg:flex-row gap-3 items-center justify-between'>
-                            <div className='flex flex-col lg:flex-row items-center gap-5'>
-                                <div className=' h-24 w-24 lg:h-32 lg:w-32 rounded-full dark:bg-white bg-black dark:text-black text-white flex justify-center items-center '>
-                                    <h1 className=' font-bold text-4xl'>{userName?.charAt(0).toUpperCase()}</h1>
-                                </div>
-                                <div className="flex flex-col items-center lg:items-start">
-                                    <h1 className='text-3xl font-semibold'>{userName}</h1>
-                                    <span className=' opacity-70 tracking-wide'>{email || "Unknown"}</span>
-                                </div>
+            {loading ? (
+                <div className="flex justify-center items-center min-h-[100dvh]">
+                    <SpinLoading />
+                </div>
+            ) : (
+                <div className="min-h-[100dvh]">
+                    <div className=" p-4 flex flex-col lg:flex-row gap-3 items-center justify-between">
+                        <div className="flex flex-col lg:flex-row items-center gap-5">
+                            <div className=" h-24 w-24 lg:h-32 lg:w-32 rounded-full dark:bg-white bg-black dark:text-black text-white flex justify-center items-center ">
+                                <h1 className=" font-bold text-4xl">{userName?.charAt(0).toUpperCase()}</h1>
                             </div>
-                            <div className="flex gap-3">
-                                <Link href={"/edit-profile"} className=' dark:bg-white bg-black p-3 dark:text-black text-white rounded-full font-semibold dark:hover:bg-transparent hover:border-2 dark:hover:border-white duration-200 dark:hover:text-white'>Manage Account</Link>
-                                <button onClick={logout} className='dark:bg-white/80 bg-black/80 dark:text-black text-white p-3 rounded-lg dark:hover:bg-transparent duration-200 dark:hover:text-white hover:scale-110'><LogOut /></button>
+                            <div className="flex flex-col items-center lg:items-start">
+                                <h1 className="text-3xl font-semibold">{userName}</h1>
+                                <span className=" opacity-70 tracking-wide">{email || "Unknown"}</span>
                             </div>
                         </div>
-                        <h1 className=' h-1 dark:bg-white/20 bg-black/20 w-full'></h1>
-
-                        <div className="flex flex-col p-5">
-                            <div className="flex justify-between items-center">
-                                <h1 className="text-3xl lg:text-5xl font-bold">History</h1>
-                                <Link href={"/history"} className="text-sm lg:text-lg">
-                                    Load more
-                                </Link>
-                            </div>
-                            <div className="flex gap-2">
-                                {/* <Cards props={Popular} /> */}
-                            </div>
-                        </div>
-
-                        <div className="flex flex-col p-4">
-                            <div className="flex justify-between items-center">
-                                <h1 className="text-3xl lg:text-5xl font-bold">Bookmark</h1>
-                                <Link href={"/bookmark"} className="text-sm lg:text-lg">
-                                    Load more
-                                </Link>
-                            </div>
-                            <div className="flex gap-2">
-                                {/* <Cards props={Popular} /> */}
-                            </div>
+                        <div className="flex gap-3">
+                            <Link href={"/edit-profile"} className=" dark:bg-white bg-black p-3 dark:text-black text-white rounded-full font-semibold dark:hover:bg-transparent hover:border-2 dark:hover:border-white duration-200 dark:hover:text-white">
+                                Manage Account
+                            </Link>
+                            <button onClick={logout} className="dark:bg-white/80 bg-black/80 dark:text-black text-white p-3 rounded-lg dark:hover:bg-transparent duration-200 dark:hover:text-white hover:scale-110">
+                                <LogOut />
+                            </button>
                         </div>
                     </div>
-                )
-            }
+                    <h1 className=" h-1 dark:bg-white/20 bg-black/20 w-full"></h1>
+
+                    <div className="flex flex-col p-5">
+                        <div className="flex justify-between items-center">
+                            <h1 className="text-3xl font-bold">History</h1>
+                            <Link href={"/history"} className="text-sm lg:text-lg">
+                                Load more
+                            </Link>
+                        </div>
+                        <div className="flex gap-2">
+                            {historyLoading ? (
+                                <div className="flex w-full justify-center items-center">
+                                    <SpinLoading />
+                                </div>
+                            ) : (
+                                <div className="hiddenscroll overflow-y-hidden m-auto  w-full grid grid-cols-2 gap-3 place-items-center md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-7 items-center  mt-8">
+                                    {history?.map((item) => (
+                                        <div key={item._id} className="border-2 border-black/20 dark:border-white/30 card-img rounded-lg">
+                                            <Link href={`/watch/${item.streamId}/${item.animeId}`} className="content-normal overflow-hidden w-full h-full">
+                                                <div className="md:w-48 h-60  relative overflow-hidden">
+                                                    <img src={item.image || "https://s4.anilist.co/file/anilistcdn/character/large/default.jpg"} alt={`an image of ${item?.animeId}`} className=" rounded-t-lg hover:scale-105 duration-200 h-60 lg:h-64 w-full " draggable="false" loading="lazy" height={400} width={200} />
+                                                </div>
+                                                <div className="flex flex-col gap-3 p-2">
+                                                    <span className="truncate font-semibold w-32 lg:w-44 text-sm md:text-xl lg:text-lg capitalize">{item.title || "Unknown Title"}</span>
+                                                    {item?.episode !== null && item?.episode !== undefined ? (
+                                                        <div className="truncate w-32 lg:w-44 text-sm lg:text-xl pb-5 capitalize flex gap-2 items-center">
+                                                            <span className=" font-semibold"> Ep: {item?.episode}</span>
+                                                        </div>
+                                                    ) : (
+                                                        <div className="truncate w-32 lg:w-44 text-sm lg:text-xl pb-5 capitalize flex gap-2 items-center">
+                                                            <span> Ep: Unknown</span>
+                                                        </div>
+                                                    )}
+                                                </div>
+                                            </Link>
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col p-4">
+                        <div className="flex justify-between items-center">
+                            <h1 className="text-3xl lg:text-5xl font-bold">Bookmark</h1>
+                            <Link href={"/bookmark"} className="text-sm lg:text-lg">
+                                Load more
+                            </Link>
+                        </div>
+                        <div className="flex gap-2">{/* <Cards props={Popular} /> */}</div>
+                    </div>
+                </div>
+            )}
         </>
+    );
+};
 
-    )
-}
-
-export default Page
+export default Page;
--- src/components/shared/navigation/SideBar.tsx
+++ src/components/shared/navigation/SideBar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { getCookie } from "cookies-next";
 import { Clapperboard, Home, TrendingUp, ScanLine, BookMarked, History, SlidersHorizontal, Diff, Tv } from "lucide-react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
@@ -51,7 +52,7 @@ const SideBar = () => {
     const pathname = usePathname();
     const [expand, setExpand] = useState(false);
     const sidebarRef = useRef<HTMLDivElement>(null);
-
+    const token = getCookie("token");
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
             if (sidebarRef.current && !sidebarRef.current.contains(event.target as Node)) {
@@ -71,6 +72,10 @@ const SideBar = () => {
                 <div className={` w-52 max-lg:w-20 h-screen bg-white/5 backdrop-blur-xl text-white sticky left-0 top-0 duration-300 border-r-2 border-black/25 dark:border-white/25`}>
                     <nav className="flex flex-col overflow-scroll h-screen mt-3 p-2 max-lg:mt-10">
                         {NavItems.map((link) => {
+                            // Check if the link is either "History" or "Bookmark" and token is not present
+                            const shouldRender = !(link.name === "History" || link.name === "Bookmark") || token;
+                            if (!shouldRender) return null;
+
                             const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
                             return (
                                 <Link href={link.route} key={link.name} className={`${isActive && `dark:bg-[#3f3f46] bg-black/10`} mb-5 duration-200  transition-all hover:scale-90 p-4 rounded-lg flex gap-3 text-lg items-center cursor-pointer dark:text-white text-black`}>
@@ -83,13 +88,12 @@ const SideBar = () => {
                 </div>
             </div>
             {/* mobile bottom bar */}
-            <div
-                ref={sidebarRef}
-                className={`text-white fixed bottom-0 md:hidden z-20 bg-white/60 dark:bg-black/50 rounded-t-xl border-t-2 dark:border-white/25 border-black/25 backdrop-blur-md w-full p-2 duration-200 transition-all`}
-            >
+            <div ref={sidebarRef} className={`text-white fixed bottom-0 md:hidden z-20 bg-white/60 dark:bg-black/50 rounded-t-xl border-t-2 dark:border-white/25 border-black/25 backdrop-blur-md w-full p-2 duration-200 transition-all`}>
                 <div className="flex items-center justify-between flex-wrap ">
                     {NavItems.slice(0, expand ? NavItems.length : 2).map((link) => {
                         const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+                        const shouldRender = !(link.name === "History" || link.name === "Bookmark") || token;
+                        if (!shouldRender) return null;
                         return (
                             <Link href={link.route} onClick={() => setExpand(false)} key={link.name} className={`flex flex-col items-center gap-3 text-sm ${isActive && ` dark:border-white/50 border-black border-2 backdrop-blur-md `} w-24 p-2 rounded-lg duration-200 transition-all`}>
                                 <link.icons className={` dark:text-white text-black ${isActive && `dark:fill-white fill-black`}`} />
@@ -97,10 +101,7 @@ const SideBar = () => {
                             </Link>
                         );
                     })}
-                    <div
-                        className="flex flex-col cursor-pointer relative items-center gap-3 text-sm w-24 p-2 rounded-lg duration-200 transition-all dark:text-white text-black"
-                        onClick={() => setExpand(!expand)}
-                    >
+                    <div className="flex flex-col cursor-pointer relative items-center gap-3 text-sm w-24 p-2 rounded-lg duration-200 transition-all dark:text-white text-black" onClick={() => setExpand(!expand)}>
                         <Diff />
                         <h1 className=" text-black dark:text-white">{expand ? "Show Less" : "Show More"}</h1>
                     </div>
