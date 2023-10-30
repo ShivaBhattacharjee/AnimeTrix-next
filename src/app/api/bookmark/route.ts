743870cdcde23e0b82bc47bb63a9560b06424fd4
Shiva <shivaxwarrior@gmail.com>
2023-10-30 18:16:12 +0530

    passed deno

--- src/app/api/bookmark/route.ts
+++ src/app/api/bookmark/route.ts
@@ -1,19 +1,19 @@
 import { connect } from "@/database/db";
 import { getDataFromJwt } from "@/helper/jwtData";
-import User from "@/model/user.model";
-import { Error } from "@/types/ErrorTypes";
+// import User from "@/model/user.model";
+// import { Error } from "@/types/ErrorTypes";
 import { NextRequest, NextResponse } from "next/server";
 
 connect();
 export async function GET(request: NextRequest) {
-    const userId = getDataFromJwt(request);
+    getDataFromJwt(request);
     return NextResponse.json({ message: "Hello World from bookmark route" });
 }
 export async function POST(request: NextRequest) {
-    const reqBody = await request.json();
+    await request.json();
     return NextResponse.json({ message: "Post request  under development" });
 }
 export async function DELETE(request: NextRequest) {
-    const reqBody = await request.json();
+    await request.json();
     return NextResponse.json({ message: "Delete request under development" });
 }
