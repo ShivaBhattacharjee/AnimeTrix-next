70324ee1cb143d72251200600ca9b37ae6a127e4
Shiva <shivaxwarrior@gmail.com>
2023-11-03 22:35:52 +0530

    Added edit profile page

---
+++ src/app/edit-profile/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+    return (
+        <div className="flex min-h-screen justify-center items-center">
+            <h1>Under Development</h1>
+        </div>
+    );
+};
+
+export default page;
