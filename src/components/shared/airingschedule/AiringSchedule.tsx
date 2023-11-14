d1affee38a22b46a5b296ba7006ec5aa41f62d4d
Shiva <shivaxwarrior@gmail.com>
2023-11-15 00:09:24 +0530

    made airing schedule client side

--- src/components/shared/airingschedule/AiringSchedule.tsx
+++ src/components/shared/airingschedule/AiringSchedule.tsx
@@ -1,16 +1,52 @@
 "use client";
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import AiringScheduleCard from "./AiringScheduleCard";
-
 import { getAiringSchedule } from "@/lib/AnimeFetch";
+import { AxiosError } from "axios"; // Import AxiosError type
+import AiringScheduleLoading from "@/components/loading/AiringScheduleLoading";
 
-export default async function AiringSchedule() {
-    const Airing = await getAiringSchedule();
+const MAX_RETRIES = 3;
+const RETRY_DELAY = 1000; // 1 second
+
+export default function AiringSchedule() {
+    const [airingData, setAiringData] = useState(null);
+    const [retryCount, setRetryCount] = useState(0);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await getAiringSchedule();
+                setAiringData(data);
+                setLoading(false);
+            } catch (error) {
+                const axiosError = error as AxiosError;
+                if (axiosError.response && axiosError.response.status === 429 && retryCount < MAX_RETRIES) {
+                    setTimeout(() => {
+                        setRetryCount(retryCount + 1);
+                        fetchData();
+                    }, RETRY_DELAY);
+                    setLoading(true);
+                } else {
+                    console.error("Error fetching airing schedule:", axiosError);
+                    setLoading(false);
+                }
+            }
+        };
+
+        fetchData();
+    }, [retryCount]);
+
     return (
-        <div className="flex flex-col mt-9">
-            <h1 className="text-3xl lg:text-5xl font-bold">Airing Schedule</h1>
-            <AiringScheduleCard airingData={Airing} />
-        </div>
+        <>
+            {loading ? (
+                <AiringScheduleLoading />
+            ) : (
+                <div className="flex flex-col mt-9">
+                    <h1 className="text-3xl lg:text-5xl font-bold">Airing Schedule</h1>
+                    {airingData && <AiringScheduleCard airingData={airingData} />}
+                </div>
+            )}
+        </>
     );
 }
