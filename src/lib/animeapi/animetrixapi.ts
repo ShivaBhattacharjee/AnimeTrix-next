a0a698537dcb445d41ebf01d79102f6d864dfad9
Shiva <shivaxwarrior@gmail.com>
2024-01-18 03:44:29 +0530

    backend is now in azure

--- src/lib/animeapi/animetrixapi.ts
+++ src/lib/animeapi/animetrixapi.ts
@@ -1,3 +1,3 @@
-export const AnimeApi = `https://api-consumet-org-six.vercel.app/meta/anilist`;
+export const AnimeApi = `http://4.224.136.120:3000/meta/anilist`;
 export const AnifyApi = `https://api.anify.tv`;
 export const StreamApi = `https://api.amvstr.me/api/v2`;
