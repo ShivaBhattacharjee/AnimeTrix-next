c1e3ed9203169b36ee5f8772f7edc763229728cc
Shiva <shivaxwarrior@gmail.com>
2024-01-18 12:36:42 +0530

    removed azure api ig

--- src/lib/animeapi/animetrixapi.ts
+++ src/lib/animeapi/animetrixapi.ts
@@ -1,3 +1,3 @@
-export const AnimeApi = `http://animeapi.animetrix.xyz:3000/meta/anilist`;
+export const AnimeApi = `https://api-consumet-org-six.vercel.app/meta/anilist`;
 export const AnifyApi = `https://api.anify.tv`;
 export const StreamApi = `https://api.amvstr.me/api/v2`;
