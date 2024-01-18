89f51898b373918db831f44fad89b8f3ef33115f
Shiva <shivaxwarrior@gmail.com>
2024-01-18 12:31:49 +0530

    azure api ig

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
+            <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
             <body className={inter.className}>
                 <section>
                     <ProfileProvider>
