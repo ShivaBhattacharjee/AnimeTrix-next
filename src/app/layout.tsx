9d77830b01b6c2740851ad69b09fd9072a5d8e0d
Shiva <shivaxwarrior@gmail.com>
2024-01-20 09:45:37 +0530

    updated package data and supressed hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
             <body className={inter.className}>
                 <section>
