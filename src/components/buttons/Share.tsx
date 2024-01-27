78416c0a07a0b8553e811ef762ac7e81eb0dc369
Shiva <shivaxwarrior@gmail.com>
2024-01-28 04:41:31 +0530

    updated(share): window to globalthis

--- src/components/buttons/Share.tsx
+++ src/components/buttons/Share.tsx
@@ -1,18 +1,26 @@
 "use client";
+
+interface CustomGlobalThis {
+    androidShare?: (title: string, text: string, url: string) => void;
+}
+
 declare global {
     interface Window {
         androidShare?: (title: string, text: string, url: string) => void;
     }
+
+    const globalThis: CustomGlobalThis;
 }
 
 import React from "react";
 import { Share2 } from "lucide-react";
 
+import Toast from "@/utils/toast";
+
 /**
  * Renders a share button that allows the user to share the current page using the Web Share API or Android Share API.
  * @param {Object} props - The component props.
  * @param {string} props.title - The title of the page to be shared.
- * @param {string} props.text - The text to be shared.
  * @returns {JSX.Element} - The rendered Share button.
  */
 const Share = ({ title }: { title: string }): JSX.Element => {
@@ -20,16 +28,16 @@ const Share = ({ title }: { title: string }): JSX.Element => {
         const shareData = {
             title: title,
             text: `Hey, I'm Watching ${title} on AnimeTrix, you would like it too! Watch it for FREE on AnimeTrix now!`,
-            url: window.location.href,
+            url: globalThis.location.href,
         };
 
         try {
             if (navigator.share) {
                 await navigator.share(shareData);
-            } else if (window.androidShare) {
-                window.androidShare(shareData.title, shareData.text, shareData.url);
+            } else if (globalThis.window && globalThis.window.androidShare) {
+                globalThis.window.androidShare(shareData.title, shareData.text, shareData.url);
             } else {
-                alert("Sharing is not supported on this device.");
+                Toast.ErrorShowToast("Your browser does not support sharing.");
             }
         } catch (error) {
             console.log(error);
@@ -37,7 +45,7 @@ const Share = ({ title }: { title: string }): JSX.Element => {
     };
 
     return (
-        <button onClick={handleShareClick} className="bg-white  p-2 rounded-lg font-semibold text-black flex items-center gap-3">
+        <button onClick={handleShareClick} className="bg-white p-2 rounded-lg font-semibold text-black flex items-center gap-3">
             <Share2 />
             Share
         </button>
