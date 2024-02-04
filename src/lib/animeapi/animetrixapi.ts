f72a739c4bcbcf477991821a5d8230cdbf0a66bf
Shiva <shivaxwarrior@gmail.com>
2024-02-04 13:56:15 +0530

    updated anime api

--- src/lib/animeapi/animetrixapi.ts
+++ src/lib/animeapi/animetrixapi.ts
@@ -1,3 +1,3 @@
-export const AnimeApi = `https://api-consumet-org-six.vercel.app/meta/anilist`;
+export const AnimeApi = `https://consumet-api-h1ga.onrender.com/meta/anilist`;
 export const AnifyApi = `https://api.anify.tv`;
 export const StreamApi = `https://api.amvstr.me/api/v2`;
