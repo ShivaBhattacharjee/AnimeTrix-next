6a09c98381434837569696902cd30804b64426cb
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2024-03-10 18:41:16 +0530

    add: loading animation

--- src/app/waifu/loading.tsx
+++ src/app/waifu/loading.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import SpinLoading from "@/components/loading/SpinLoading";
+
 const loading = () => {
-    return <div>loading</div>;
+    return (
+        <div className="flex min-h-[70vh] flex-col justify-center items-center">
+            <SpinLoading />
+        </div>
+    );
 };
 
 export default loading;
