1342b24e131d8a80022f09a97749633d2b04836c
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2024-05-22 12:19:52 +0530

    add: choose custom profile picture

--- src/components/shared/AvatarModal.tsx
+++ src/components/shared/AvatarModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, UserRoundCog } from "lucide-react";
 
 import { ProfiePicture } from "@/utils/ProfilePicture";
 
@@ -11,10 +11,29 @@ interface AvatarModalProps {
 }
 
 const AvatarModal: React.FC<AvatarModalProps> = ({ setOpenAvatarModal, onSelectProfilePicture, openAvatarModal }) => {
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                const base64String = reader.result as string;
+                onSelectProfilePicture(base64String);
+                setOpenAvatarModal(false);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
     return (
         <>
             <div className={`absolute w-full z-50 p-4 left-0 right-0 bg-black bottom-0 ${openAvatarModal ? "top-0" : "top-[2000px] hidden"} duration-200 overflow-y-scroll min-h-screen hiddenscroll `}>
                 <ArrowLeft className="cursor-pointer" onClick={() => setOpenAvatarModal(false)} size={40} />
+                <h1 className="mt-7 text-2xl lg:text-3xl font-semibold">Custom</h1>
+                <div className="w-24 mt-5 relative h-24 text-black lg:w-32 lg:h-32 rounded-full bg-white flex justify-center items-center">
+                    <div className="flex relative cursor-pointer w-full justify-center items-center">
+                        <input type="file" className=" opacity-0 z-50 cursor-pointer" accept=".png , .jpg , .jpeg" onChange={handleFileChange} />
+                        <UserRoundCog className=" text-8xl absolute scale-150 lg " />
+                    </div>
+                </div>
                 {Object.keys(ProfiePicture.Profiles).map((anime) => (
                     <div key={anime}>
                         <h1 className="mt-7 text-2xl lg:text-3xl font-semibold">{anime}</h1>
